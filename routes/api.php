6dffc95119f160d7098a2b4e81c8a2b720cd0331
Cristhian Kelm <criskp007@gmail.com>
2022-05-26 20:49:35 -0400

    create routes

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,55 @@ use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\WallController;
 use App\Http\Controllers\Api\WarningController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
+
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/auth/validate', [AuthController::class, 'validate']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    //Mural de Avisos
+    Route::get('/walls', [WallController::class, 'getAll']);
+    Route::get('/walls/{id}/like', [WallController::class, 'like']);
+
+    //Documentos
+    Route::get('/docs', [DocController::class, 'getAll']);
+
+    //Livro de ocorrências
+    Route::get('/warnings', [WarningController::class, 'getMyWarnings']);
+    Route::post('/warning', [WarningController::class, 'setWarning']);
+    Route::post('/warning/file', [WarningController::class, 'addWarningFile']);
+
+    //Boletos
+    Route::get('/billets', [BilletController::class, 'getAll']);
+
+    //Achados e Perdidos
+    Route::get('/found-and-lost', [FoundAndLostController::class, 'getAll']);
+    Route::get('/found-and-lost', [FoundAndLostController::class, 'insert']);
+    Route::put('/found-and-lost/{id}', [FoundAndLostController::class, 'update']);
+
+    //Unidade
+    Route::get('/unit/{id}', [UnitController::class, 'getInfo']);
+    Route::post('/unit/{id}/add-person', [UnitController::class, 'addPerson']);
+    Route::post('/unit/{id}/add-vehicle', [UnitController::class, 'addVehicle']);
+    Route::post('/unit/{id}/add-pet', [UnitController::class, 'addPet']);
+    Route::delete('/unit/{id}/remove-person', [UnitController::class, 'removePerson']);
+    Route::delete('/unit/{id}/remove-vehicle', [UnitController::class, 'removeVehicle']);
+    Route::delete('/unit/{id}/remove-pet', [UnitController::class, 'removePet']);
+
+    //Reservas
+    Route::get('/reservations', [ReservationController::class, 'getReservations']);
+    Route::post('/reservation/{id}', [ReservationController::class, 'setReservation']);
+
+    Route::get('/reservation/{id}/disabled-dates', [ReservationController::class, 'getDisabledDays']);
+    Route::get('/reservation/{id}/times', [ReservationController::class, 'getTimes']);
+
+    Route::get('/my-reservations', [ReservationController::class, 'getMyReservations']);
+    Route::delete('/my-reservation/{id}', [ReservationController::class, 'delMyReservations']);
+});
