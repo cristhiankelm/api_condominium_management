48d3c9cdf5af3c5072aa85c5780b53a260ea1e70
Cristhian Kelm <criskp007@gmail.com>
2022-05-28 21:28:06 -0400

    add functiond getAll on WallController

--- app/Http/Controllers/Api/WallController.php
+++ app/Http/Controllers/Api/WallController.php
@@ -5,7 +5,38 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\User;
+use App\Models\Api\Wall;
+use App\Models\Api\WallLike;
+
 class WallController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => '', 'list' => []];
+
+        $user = auth()->user();
+
+        $walls = Wall::all();
+
+        foreach ($walls as $wallKey => $wallValue) {
+            $walls[$wallKey]['likes'] = 0;
+            $walls[$wallKey]['liked'] = false;
+
+            $likes = WallLike::where('wall_id', $wallValue['id'])->count();
+            $walls[$wallKey]['likes'] = $likes;
+
+            $meLikes = WallLike::where('wall_id', $wallValue['id'])
+                ->where('user_id', $user['id'])
+                ->count();
+
+            if ($meLikes > 0) {
+                $walls[$wallKey]['liked'] = true;
+            }
+        }
+
+        $array['list'] = $walls;
+
+        return $array;
+    }
 }
