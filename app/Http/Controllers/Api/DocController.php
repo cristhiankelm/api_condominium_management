c34ef1d98f1d593a7211d9bd5a08a7124f8e5ff3
Cristhian Kelm <criskp007@gmail.com>
2022-05-28 22:53:59 -0400

    add functiond getAll on DocController

--- app/Http/Controllers/Api/DocController.php
+++ app/Http/Controllers/Api/DocController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+use App\Models\Api\Doc;
 
 class DocController extends Controller
 {
-    //
+    public function getAll()
+    {
+        $array = ['error' => ''];
+
+        $docs = Doc::all();
+
+        foreach ($docs as $docKey => $docValue){
+            $docs[$docKey]['file'] = asset('storage/' . $docValue['file']);
+        }
+
+        $array['list'] = $docs;
+
+        return $array;
+    }
 }
